b5d16477b782a5bb7b318a7c1ba24e4f528b38fb
avantri <artjerom@gmail.com>
2017-07-29 14:38:47 +0300

    1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,6 +7,33 @@ function handleChangeContentPost(e) {
     $(container).find('.js_show_text').html(val);
 }
 
+$(document).ready(function () {
+    $('.save').on('click', function (e) {
+        var canvas = document.getElementById("canvas"),
+            html = $('.image').html();
+
+        console.log(html);
+
+        rasterizeHTML.drawHTML(html, canvas);
+    });
+/*    $(".save").on('click', function (e) {
+        html2canvas($(".bottom-img"), {
+            onrendered: function(canvas) {
+                theCanvas = canvas;
+                document.body.appendChild(canvas);
+
+                // Convert and download as image
+                Canvas2Image.saveAsPNG(canvas);
+                $("#img-out").append(canvas);
+/!*
+                var myImage = canvas.toDataURL("image/png");
+                $('#out').attr('src', myImage);
+*!/
+            }
+        });
+    });*/
+});
+
 // Получаем изображение и вставляем в вартермарк
 function readUrl(input) {
     if (input.files && input.files[0]) {
